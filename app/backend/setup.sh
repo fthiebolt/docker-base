9f90be48f2501fceb93466671de9a8f80ae6182e
Francois <thiebolt@irit.fr>
2021-06-04 10:42:40 +0200

    update

---
+++ app/backend/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Sample myApp setup script
+#
+
+set -e
+
+echo -e "\nSetting up things ..."
+env
+
---
+++ app/dashboard/application.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Sample myApp run script
+#
+
+set -e
+
+echo -e "\nLaunching my $0 application things ..."
+env
+
+# this code means normal end of my application
+exit 0
+
+
